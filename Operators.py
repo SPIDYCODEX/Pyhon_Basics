5a31f63f0ed4b2e1ae8a0e964f5dd9d7bb25b22e
SPIDYCODER🕷 <uditgurudia420@gmail.com>
2025-06-07 19:53:16 +0530

    Add files via upload
    Operators in Python🐍

---
+++ Operators.py
@@ -0,0 +1,99 @@
+# # Arithmatic Operators
+a =4
+b=6
+sum= a+b
+print(a+b)
+print(sum)
+r=55
+t=33
+print(r-t)
+i=9
+j=0
+print(i*j)
+
+# # Check the type of Variable
+print(type(sum))   
+print(type(a))
+print(type(b))
+c=20
+d=2
+print(c/d)
+
+# # Assignment Operators(true / false)
+A=4
+B=4
+print(A + B)
+A+=3     #increament of 3 in A value(A=3+3)
+
+B-=2     #Decreament of 2 in B value(B=4-2)
+print(A)
+print(B)
+# Comparision Operators (True/False)
+W=44
+X=44
+print(W==X)
+print(W!=X)
+print((W>X))
+print(W<X)
+# # Logical Operators (OR, NOT, AND )
+v= True or False #or operator
+print(v)
+print(False or True)
+print(True or True)
+print(False or False)
+
+w = True and False # And operator
+print(w)
+print(False and True)
+print(False and False)
+print(True and False)
+print(True and True)
+
+# Not operator
+print(not(True))
+print(not(False))
+
+# Type of Variable
+Int=344
+print(type(Int))
+FLoat=44.4
+print(type(FLoat))
+String="Spidy🕷"
+print(type(String))
+Boolean=True or False
+print(type(Boolean))
+NOne=0
+print(NOne)
+print(type(NOne))
+inte= float(40)
+print(inte)
+#INPUT VARIABLE  DATA TYPES IN PYTHON:
+h= int(input ("Enter a NUmber of a:"))
+z= float(input ("Enter a NUmber of b:"))
+print("The Number Is:",h)
+print("The Number Is:",z)
+print(h+z)    # Input always in String type
+
+
+#NOW THE PROBLEM SOLVE TASK FROM THE LESSON 
+# ADD TWO NUMBER WHICH IS GIVEN Q=34, P=44
+Q=34
+P=44
+print(Q+P)
+# REMINDER CHECK 
+x=55
+print("The Number reminder is :", x % 5)
+#COMPARISION OPERATOR CHECK
+a=34
+b=60
+print(a>b)
+print(a<b)
+#AVERAGE VALUE OF TWO NUMBERS
+Integer=int(input("Enter a Num "))
+Integer2=int(input("Enter a num "))
+print("The average Num of two variable is :", (Integer+Integer2 )/2)
+
+
+#SQUARE OF A NUMBER
+d=int(input("Enter the value:"))
+print("The square of the Value is :",d*d)
